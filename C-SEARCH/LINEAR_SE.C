df5f5a6189d96eefd8707c17d284eb7168b929e5
SaurabhKhadtare <saurabhsk13121996@gmail.com>
2016-12-18 13:37:06 +0530

    Add files via upload
    This is Searching Methods in C language..!!

---
+++ C-SEARCH/LINEAR_SE.C
@@ -0,0 +1,31 @@
+//program  for linear search                             roll no:153108
+
+#include<stdio.h>
+#include<conio.h>
+#define MAX 50
+void search(int arr[],int);
+void main()
+{
+int arr[MAX],i,n;
+clrscr();
+printf("\n enter how many numbers");
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+search(arr,n);
+getch();
+}
+void search(int arr[],int n)
+{
+int i,temp;
+printf("\n enter the numbers to find");
+scanf("%d",& temp);
+for(i=0;i<n;i++)
+{
+if(arr[i]==temp)
+printf("number found at %d position",i+1);
+break;
+}
+if(i>n)
+printf("\n number not found");
+getch();
+}
